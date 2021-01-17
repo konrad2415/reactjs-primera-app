d08ed47de0658fce4025def93c4ab9a0bad3eb85
Daibel Inle Martinez [Konrad Zuse] <konraduse.dm@gmail.com,konradzus@nauta.cu>
2021-01-17 11:45:06 -0500

    hotfix-on develop branch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,92 @@
-import React ,{ useState } from 'react';
-import "./style/App.css";
-import Menu from './menu'
-import List from './list'
-import star  from './img/star.png'
+import React from 'react';
+import './App.css';
+import Menu from './menu';
+import List from './list';
 
- function App(){
+class App extends React.Component{
 
-   // Array with the items at initial state 
-   const initialState='{ "books":['+
-   '{id:0, rating: 4, title: "Harry Potter y el Cadis de Fuego" , image: "libro01.jpg"},'+
-   '{id:0, rating: 3, title: "The Shining" , image: "libro02.jpg"},'+
-   '{id:0, rating: 5, title: "El Código Da Vinci" , image: "libro03.jpg"},'+
-   '{id:0, rating: 5, title: "El Principito" , image: "libro04.jpg"},'+
-   '{id:0, rating: 5, title: "Sobrenatural" , image: "libro05.jpg"}]}';
-   
-   
-   const [state,setState]= useState(initialState);
-   const { books } = state;
-   <List items={books} />
+  constructor(props){
+    super(props);
+    this.state = {
+      books:[
+        {id:0, rating: 4, title: 'Harry Potter y el cáliz de fuego', image: 'libro01.jpg'},
+        {id:1, rating: 3, title: 'The shining', image: 'libro02.jpg'},
+        {id:2, rating: 5, title: 'Código Da Vinci', image: 'libro03.jpg'},
+        {id:3, rating: 5, title: 'El principito', image: 'libro04.jpg'},
+        {id:4, rating: 5, title: 'Sobrenatural', image: 'libro05.jpg'}
+      ],
+      copyBooks: []
+    };
+
+    this.onSearch = this.onSearch.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.remove = this.remove.bind(this);
+    this.updateRating = this.updateRating.bind(this);
    }
-    return(
-     <div className="App">
-        <Menu title="Amase" />
-        <List items={books} />
-     </div>
-    );
- }
 
- export default App;
+  initBooks(){
+    //this.setState({copyBooks: [...this.state.books]});
+    this.setState((state,props) => ({
+      copyBooks: [...state.books]
+    }));
+  }
+
+  componentDidMount(){
+    this.initBooks();
+  }
+
+  onSearch(query){
+    if(query === ''){
+      this.setState({copyBooks: [...this.state.books]});
+    }else{
+
+      const temp = [...this.state.books];
+      var res = [];
+      temp.forEach(item =>{
+        if(item.title.toLowerCase().indexOf(query) > -1){
+          res.push(item);
+        }
+      });
+    
+      this.setState({copyBooks: [...res]});
+    }
+  }
+
+  addItem(item){
+    var temp = [...this.state.books];
+    const id = temp[temp.length-1].id + 1;
+    item['id'] = id;
+    temp.push(item);
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+
+  remove(id){
+    var temp = [...this.state.books];
+    const res = temp.filter(item => item.id != id);
+    this.setState({books: [...res]});
+    this.initBooks();
+  }
+
+  updateRating(item){
+    var temp = [...this.state.books];
+    const index = temp.findIndex(x => x.id === item.id);
+    temp[index].title = item.title;
+    temp[index].image = item.image;
+    temp[index].rating = item.rating;
+
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+
+  render(){
+    return (
+      <div className="app">
+        <Menu title="Amozon" onsearch={this.onSearch} onadd={this.addItem} />
+        <List className="list" items={this.state.copyBooks} onremove={this.remove} onupdaterating={this.updateRating} />
+      </div>
+    );
+  }
+}
+
+export default App;
